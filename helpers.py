6d77f6123d953c5b04c9c07563f7d3e1645367c2
shajiwilliam <90017944+shajiwilliam@users.noreply.github.com>
2023-11-19 22:51:50 -0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -40,6 +40,7 @@ def compute_accuracy(eval_preds: EvalPrediction):
 # and finding the right offsets for the answer spans in the tokenized context (to use as labels).
 # Adapted from https://github.com/huggingface/transformers/blob/master/examples/pytorch/question-answering/run_qa.py
 def prepare_train_dataset_qa(examples, tokenizer, max_seq_length=None):
+    print("prepare_train_dataset_qa",examples)
     questions = [q.lstrip() for q in examples["question"]]
     max_seq_length = tokenizer.model_max_length
     # tokenize both questions and the corresponding context
