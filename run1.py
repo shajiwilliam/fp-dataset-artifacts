9d3690198953c487c597713c3794aeb84981460a
shajiwilliam <90017944+shajiwilliam@users.noreply.github.com>
2023-11-19 23:53:37 -0600

    Update run1.py

--- run1.py
+++ run1.py
@@ -61,8 +61,8 @@ def main():
         #combined_eval_dataset = datasets.concatenate_datasets([squad_dataset['validation'], adv_squad_dataset['validation']])
         combined_eval_dataset = datasets.concatenate_datasets([squad_dataset['validation'], adv_squad_dataset['train']])
 
-        for key, value in squad_dataset['validation'].items():
-            print("Printing orig squad validation ", key)
+        squad_get = load_dataset("squad", split="validation")        
+        squad_get.to_json("squad.json")     
         
         #dataset = {'train': combined_train_dataset, 'validation': combined_eval_dataset}
         #eval_split = 'validation'
